901848a0697b6f5d22889569c9c1dfefabca103b
toptrendinginfo <68633941+toptrendinginfo@users.noreply.github.com>
2020-07-22 23:42:26 +0530

    Update Common.js

--- src/Common.js
+++ src/Common.js
@@ -1,34 +1,30 @@
 import React from "react";
-import web from "../src/assets/images/Robot.png";
+
 import { NavLink } from "react-router-dom";
 
 const Common = (props) => {
   return (
     <>
-      <section id="header" className="d-flex align-items-center mt-5">
-        <div className="container-fluid mt-5">
+      <section id="header" className="d-flex align-items-center">
+        <div className="container">
           <div className="row">
-            <div className="col-10 mx-auto">
-              <div className="row">
-                <div className="col-md-6 mt-3 pt-1 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column ">
-                  <h1>
-                    {props.name}
-                    <br />
-                    <strong className="brand-name"> Top Trending Info</strong>
-                  </h1>
-                  <h2 className="my-1">
-                    Your One Stop Solution for Tech Gadgets
-                  </h2>
-                  <div className="mt-1">
-                    <NavLink to={props.visit} className="btn-get-started">
-                      {props.btname}
-                    </NavLink>
-                  </div>
-                </div>
-                <div className="col-md-6 pt-2 pt-lg-0 order-2 order-lg-1 header-img">
-                  <img src={props.imgsrc} className="img-fluid animated"></img>
-                </div>
+            <div className="col-md-6 pt-1 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column ">
+              <h1>{props.name}</h1>
+              <p className="brand-name"> Top Trending Info</p>
+
+              <h2 className="my-1">Your One Stop Solution for Tech Gadgets</h2>
+              <div className="mt-2">
+                <NavLink to={props.visit} className="btn-get-started">
+                  {props.btname}
+                </NavLink>
               </div>
+            </div>
+            <div className="col-md-6  pt-2 pt-lg-0 order-2 order-lg-1 header-img">
+              <img
+                src={props.imgsrc}
+                className="img-fluid animated"
+                alt="imageslide"
+              />
             </div>
           </div>
         </div>
